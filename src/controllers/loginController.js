1d67e476d84d0f1d85e3dea81e987482c126f1c9
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-11-20 12:42:09 -0300

    login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,20 @@
-const Login = require('../mo')
+const Login = require('../models/LoginModel')
 exports.index = (req, res) => {
     res.render('login');
   };
   
- exports.register = (req, res) => {
-    res.send('req.body');
+ exports.register = async (req, res) => {
+    const login = new Login(req.body)
+    await login.register(); // registra e valida os campos
+
+    if(login.errors.length > 0){
+        req.flash('errors', login.errors);
+        req.session.save(function(){
+           return res.redirect('back');
+        })
+        return;
+    }
+    res.send(login.errors);
+
+    // res.send(login.body);
   };
--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const LoginSchema = new mongoose.Schema({
-  titulo: { type: String, required: true },
-  descricao: String
+  email: { type: String, required: true },
+  senha: { type: String, required: true },
+
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
@@ -10,7 +12,45 @@ const LoginModel = mongoose.model('Login', LoginSchema);
 class Login {
 constructor(body){
   this.body = body;
+  this.errors = []; // se pode o usuario ser criado ou nao no banco de dados
+  this.user = null;
 }
+
+async register(){ // promessa tem q ter try e catch
+  this.valida(); 
+  if(this.errors.length > 0) return;
+  try{
+    this.user = await LoginModel.create(this.body) // await = espera
+  } catch(err){
+    console.log(err);
+  }
+}
+
+valida(){
+  this.cleanUp();
+  //validação
+  //o e-mail precisa ser valido
+  if(!validator.isEmail(this.body.email)){
+    this.errors.push('Email inválido')
+  }
+  //a senha precisa ser  entre 6 a 50 caracteres
+  if(this.body.password.length < 6 || this.body.password.length > 50){
+    this.errors.push('A senha precisa ter entre 6 e 50 caracteres')
+  }
+}
+
+cleanUp() { 
+  //garante que tudo que recebe é uma string
+  for(const key in this.body) {
+     if(typeof this.body[key] !== 'string'){
+      this.body[key] = '';
+     } 
+  };
+  this.body = {
+    email: this.body.email,
+    password: this.body.password
+  }
+};
 }
 
 module.exports = Login;
