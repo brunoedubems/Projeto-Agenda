698a09af8f8577e0fa9780f7b2cc899b0f785b4f
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-11-21 20:16:22 -0300

    login e contato
    ainda precisa de alguns ajustes no cadastramento do contatos

--- routes.js
+++ routes.js
@@ -3,14 +3,23 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middlewares/middleware');
+
 
 // Rotas da home
 route.get('/', homeController.index);
 
-//rotas de login
-route.get('/login/index',loginController.index)
-route.post('/login/register',loginController.register)
+// Rotas de login
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
 
-
+//rotas de contato
+route.get('/contato/index',loginRequired, contatoController.index);
+route.post('/contato/register',loginRequired, contatoController.index);
+route.get('/contato/index/:id',loginRequired, contatoController.editIndex);
 
 module.exports = route;
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -20,3 +20,14 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+
+exports.loginRequired = (req, res, next) => {
+  if(!req.session.user) {
+    req.flash('errors', 'Você precisa fazer o Login')
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+
+  next();
+};
