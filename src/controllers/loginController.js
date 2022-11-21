3edc42b555367a3d02a29b3b33202bdebe3357dc
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-11-21 14:53:56 -0300

    cadastro

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,28 +1,31 @@
 const Login = require('../models/LoginModel')
+
 exports.index = (req, res) => {
     res.render('login');
   };
   
- exports.register = async (req, res) => {
+ exports.register = async function(req, res) {
     try{
         const login = new Login(req.body)
         await login.register(); // registra e valida os campos
     
         if(login.errors.length > 0){
             req.flash('errors', login.errors);
-            req.session.save(function(){
-               return res.redirect('back');
+            req.session.save(
+                function(){
+                    return res.redirect('/login/index');
             });
             return;
         }
         //quando o usuario for criado com sucesso
-        req.flash('success', 'usuario criado com sucess');
+        req.flash('success', 'Usuário criado com sucess');
         req.session.save(function(){
            return res.redirect('back');
         });
+        //return res.send(login.errors);
     } catch(e) {
         console.log(e)
-   return  res.render('404')
+        return  res.render('404')
     }
 
   };
--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -1,56 +1,82 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  password: { type: String, required: true },
-
+  password: { type: String, required: true }
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
 
 class Login {
-constructor(body){
-  this.body = body;
-  this.errors = []; // se pode o usuario ser criado ou nao no banco de dados
-  this.user = null;
-}
+  constructor(body) {
+    this.body = body;
+    this.errors = [];
+    this.user = null;
+  }
 
-async register(){ // promessa tem q ter try e catch
-  this.valida(); 
-  if(this.errors.length > 0) return;
-  try{
-    this.user = await LoginModel.create(this.body) // await = espera
-  } catch(err){
-    console.log(err);
-  }
-}
+  async login() {
+    this.valida();
+    if(this.errors.length > 0) return;
+    this.user = await LoginModel.findOne({ email: this.body.email });
 
-valida(){
-  this.cleanUp();
-  //validação
-  //o e-mail precisa ser valido
-  if(!validator.isEmail(this.body.email)){
-    this.errors.push('Email inválido')
-  }
-  //a senha precisa ser  entre 6 a 50 caracteres
-  if(this.body.password.length < 6 || this.body.password.length > 50){
-    this.errors.push('A senha precisa ter entre 6 e 50 caracteres')
-  }
-}
+    if(!this.user) {
+      this.errors.push('Usuário não existe.');
+      return;
+    }
 
-cleanUp() { 
-  //garante que tudo que recebe é uma string
-  for(const key in this.body) {
-     if(typeof this.body[key] !== 'string'){
-      this.body[key] = '';
-     } 
-  };
-  this.body = {
-    email: this.body.email,
-    password: this.body.password
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push('Senha inválida');
+      this.user = null;
+      return;
+    }
   }
-};
+
+  async register() {
+    this.valida();
+    if(this.errors.length > 0) return;
+
+    await this.userExists();
+
+    if(this.errors.length > 0) return;
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+    this.user = await LoginModel.create(this.body);
+  }
+
+  async userExists() {
+    this.user = await LoginModel.findOne({ email: this.body.email });
+    if(this.user) this.errors.push('Usuário já existe.');
+  }
+
+  valida() {
+    this.cleanUp();
+
+    // Validação
+    // O e-mail precisa ser válido
+    if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+
+    // A senha precisa ter entre 3 e 50
+    if(this.body.password.length < 3 || this.body.password.length > 50) {
+      this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
+    }
+  }
+
+  cleanUp() {
+    for(const key in this.body) {
+      if(typeof this.body[key] !== 'string') {
+        this.body[key] = '';
+      }
+    }
+
+    this.body = {
+      email: this.body.email,
+      password: this.body.password
+    };
+  }
 }
 
 module.exports = Login;
